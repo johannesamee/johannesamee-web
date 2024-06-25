f1dd9450e063464c21b9cd1e9c95ad089c543427
johannesbjorkdahl-decerno <jbjorkd1@volvocars.com>
2024-06-25 23:59:45 +0200

    Adding Vercel SpeedInsights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,11 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
+
 export const prerender = true
 
 export async function load({ url }) {
-	return {
-		url: url.pathname
-	}
+  return {
+    url: url.pathname
+  }
 }
